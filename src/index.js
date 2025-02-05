c3fde01a5efd502ab21357e28db7e3ec1f67ac1f
Venkat varma <kvenkatvarma@gmail.com>
2025-02-05 12:50:47 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function MainBody(){
+  return(
+    <div>
+      <p>In this course, we will learn react js by building Taskopedia</p>
+      <ul>
+        <li>Call Ben</li>
+        <li>Go to walmart</li>
+      </ul>
+    </div>
+  )
+}
+
+function MainHeader(){
+  return(
+    <h1>REACT COURSE</h1>
+  )
+}
+
+function Header(){
+  return(
+    <div>
+          <MainHeader />
+          <SubHeader />
+    </div>
+ 
+  )
+}
+
+function SubHeader(){
+  return(
+    <p>This will be an exciting course</p>
+  )
+}
+
+function Footer(){
+  return(
+    <p>Happy Coding!</p>
+  )
+}
+root.render(
+ <div>
+  <Header />
+    <MainBody/>
+    <Footer />
+ </div>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
