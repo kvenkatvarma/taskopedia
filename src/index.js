12211a63b100ff77eebcb173ea4f596b91fb36d8
Venkat varma <kvenkatvarma@gmail.com>
2025-02-05 14:02:04 +0530

    Students Component separation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import "./CSS/style.css";
-import Header from './header';
+import Header from './Header';
+import Students from './Students';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -21,26 +22,6 @@ function MainBody(){
 }
 
 
-function Students(){
-  const fullName = "Venkat varma";
-  const programmingExp = 2;
-  return(
-    <div className='container p-4'>
-       <div className='row'>
-         Students Enrolled
-         </div>
-         <div className='row border'>
-             <div className='col-2'>
-               <img src={`https://ui-avatars.com/api/?name= ${fullName}`}></img>
-             </div>
-             <div className='col-10'>
-                {fullName} <br/>
-                Coding Experience {programmingExp} years
-             </div>
-         </div>
-    </div>
-  )
-}
 function Footer(){
   return(
     <p style={{color:"gray",backgroundColor:"black"}}>Happy Coding!</p>
