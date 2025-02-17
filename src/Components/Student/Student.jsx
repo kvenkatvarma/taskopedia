2d51ad4aabab7f60f82b6552445c6ccb008e0673
Venkat varma <kvenkatvarma@gmail.com>
2025-02-17 10:17:41 +0530

    Class Components

--- src/Components/Student/Student.jsx
+++ src/Components/Student/Student.jsx
@@ -1,19 +1,24 @@
-const Student =(props) =>{    
+import React,{Component} from "react";
+
+class Student extends React.Component{    
+  render()
+  {
     return(
       <div className='col-4 p-1'>        
            <div className='row border'>
                <div className='col-2'>
-                 <img src={props.headshot} className="w-100 py-2"></img>
+                 <img src={this.props.headshot} className="w-100 py-2"></img>
                </div>
                <div className='col-8'>
-                  {props.name} <br/>
-                  Coding Experience {props.experience} years
+                  {this.props.name} <br/>
+                  Coding Experience {this.props.experience} years
                </div>
                <div className="col-2">
-                  {props.children}
+                  {this.props.children}
                </div>
            </div>
       </div>
-    )
+    );
+  }
   }
   export default Student;
--- src/Components/Student/StudentReview.jsx
+++ src/Components/Student/StudentReview.jsx
@@ -1,9 +1,13 @@
-const StudentReview= ()=>{
-    return(
+import React,{Component} from "react"
+class StudentReview extends React.Component{
+    render()
+    {
+     return(
            <div className="p-2">
                    <i class="bi bi-hand-thumbs-up-fill text-success" style={{cursor:"pointer"}}></i> &nbsp;
                    <i class="bi bi-hand-thumbs-down-fill text-danger" style={{cursor:"pointer"}}></i>
            </div>
     )
+}
 }
 export default StudentReview;
